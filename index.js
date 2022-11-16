e8d155ee131f9a7fae6d1de65eb21e77e5bf53fd
nikita-volgin <volga.sk8@gmail.com>
2022-11-16 15:55:47 +0600

    Update index.js
    fixed problem with axios

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const { default: axios } = require('axios')
+const axios = require('axios')
 
 const app = require('express')(),
     bodyparser = require('body-parser')
 
 const botToken = 'bot5671144029:AAEQ1QSnofMwGLJgnSO_MMBgodG7y52Os9M'
-const port = process.env.PORT || 80
 
 app.use(bodyparser.json())
 
-app.listen(port, () => {
-    console.log('bot is listening http://localhost:3000')
+app.listen(process.env.PORT || 8080, () => {
+    console.log('bot is listening')
 })
 
 app.post('/', async (req, res) => {
     const chatId = req.body.message.chat.id
     const message = req.body.message.text
+
+    console.log({chatId, message});
 
     await sendMessage(chatId, message)
 
