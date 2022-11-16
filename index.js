4534c6cf19d3717affd7eaa67dadde60244f4ab5
nikita-volgin <volga.sk8@gmail.com>
2022-11-16 15:22:19 +0600

    Update index.js
    added port

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const app = require('express')(),
     bodyparser = require('body-parser')
 
 const botToken = 'bot5671144029:AAEQ1QSnofMwGLJgnSO_MMBgodG7y52Os9M'
+const port = process.env.PORT || 80
 
 app.use(bodyparser.json())
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('bot is listening http://localhost:3000')
 })
 
 app.post('/', async (req, res) => {
-    await sendMessage(791669874, 'hello, world')
+    const chatId = req.body.message.chat.id
+    const message = req.body.message.text
+
+    await sendMessage(chatId, message)
 
     res.sendStatus(200)
 })
