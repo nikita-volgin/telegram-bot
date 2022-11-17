45745440fbbbccb8bf5227936677b286ba62b0ff
nikita-volgin <volga.sk8@gmail.com>
2022-11-17 11:05:03 +0600

    Update index.js
    testing an error when changing a message

--- index.js
+++ index.js
@@ -8,21 +8,25 @@ const botToken = 'bot5671144029:AAEQ1QSnofMwGLJgnSO_MMBgodG7y52Os9M'
 app.use(bodyparser.json())
 
 app.post('/', async (req, res) => {
-    const chatId = req.body.message.chat.id
+    const chatId = req.body.message.chat ? req.body.message.chat.id : '791669874'
 
-    const message = req.body.message.sticker ? `sticker ${req.body.message.sticker.emoji}` :
-        req.body.message.voice ? "voice messages" :
-        req.body.message.text.toLowerCase()
+    const message = JSON.parse(req.body.message)
 
-    if (message === '/start') {
-        await sendMessage(chatId, 'welcome to the club, buddy')
-    } else if (message === 'hi') {
-        await sendMessage(chatId, 'wassup, man')
-    } else if (message === 'hello') {
-        await sendMessage(chatId, 'oh, so official')
-    } else {
-        await sendMessage(chatId, `i don't know what u need, fuck to your '${message}', have a nice day`)
-    }
+    await sendMessage(chatId, 'message')
+
+    // const message = req.body.message.sticker ? `sticker ${req.body.message.sticker.emoji}` :
+    //     req.body.message.voice ? "voice messages" :
+    //     req.body.message.text.toLowerCase()
+
+    // if (message === '/start') {
+    //     await sendMessage(chatId, 'welcome to the club, buddy')
+    // } else if (message === 'hi') {
+    //     await sendMessage(chatId, 'wassup, man')
+    // } else if (message === 'hello') {
+    //     await sendMessage(chatId, 'oh, so official')
+    // } else {
+    //     await sendMessage(chatId, `i don't know what u need, fuck to your '${message}', have a nice day`)
+    // }
 
     res.sendStatus(200)
 })
